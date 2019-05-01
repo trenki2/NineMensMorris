c6799bc1987bb752c5c5a13f52405403f6d78c0f
Markus Trenkwalder <trenki2@gmx.net>
2019-05-01 16:23:02 +0200

    Initial commit

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    private struct BoardElement
+    {
+        public int Player { get; set; }
+        public GameObject GameObject { get; set; }
+        public Vector2 Position { get; set; }
+    }
+
+    private List<BoardElement> elements = new List<BoardElement>();
+
+    private void Start()
+    {
+        var pos0 = GameObject.Find("BoardPos0").transform.position;
+        var pos1 = GameObject.Find("BoardPos1").transform.position;
+        var pos2 = GameObject.Find("BoardPos2").transform.position;
+
+        elements.Add(new BoardElement { Position = pos0 });
+        elements.Add(new BoardElement { Position = new Vector3(0, 0, pos0.z) });
+        elements.Add(new BoardElement { Position = new Vector3(-pos0.x, 0, pos0.z) });
+        elements.Add(new BoardElement { Position = new Vector3(-pos0.x, 0, 0) });
+        elements.Add(new BoardElement { Position = new Vector3(-pos0.x, 0, -pos0.z) });
+        elements.Add(new BoardElement { Position = new Vector3(0, 0, -pos0.z) });
+        elements.Add(new BoardElement { Position = new Vector3(pos0.x, 0, pos0.z) });
+        elements.Add(new BoardElement { Position = new Vector3(pos0.x, 0, 0) });
+
+        elements.Add(new BoardElement { Position = pos1 });
+        elements.Add(new BoardElement { Position = new Vector3(0, 0, pos1.z) });
+        elements.Add(new BoardElement { Position = new Vector3(-pos1.x, 0, pos1.z) });
+        elements.Add(new BoardElement { Position = new Vector3(-pos1.x, 0, 0) });
+        elements.Add(new BoardElement { Position = new Vector3(-pos1.x, 0, -pos1.z) });
+        elements.Add(new BoardElement { Position = new Vector3(0, 0, -pos1.z) });
+        elements.Add(new BoardElement { Position = new Vector3(pos1.x, 0, pos1.z) });
+        elements.Add(new BoardElement { Position = new Vector3(pos1.x, 0, 0) });
+
+        elements.Add(new BoardElement { Position = pos2 });
+        elements.Add(new BoardElement { Position = new Vector3(0, 0, pos2.z) });
+        elements.Add(new BoardElement { Position = new Vector3(-pos2.x, 0, pos2.z) });
+        elements.Add(new BoardElement { Position = new Vector3(-pos2.x, 0, 0) });
+        elements.Add(new BoardElement { Position = new Vector3(-pos2.x, 0, -pos2.z) });
+        elements.Add(new BoardElement { Position = new Vector3(0, 0, -pos2.z) });
+        elements.Add(new BoardElement { Position = new Vector3(pos2.x, 0, pos2.z) });
+        elements.Add(new BoardElement { Position = new Vector3(pos2.x, 0, 0) });
+    }
+}
---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    public int player;
+
+    private bool dragging;
+    private Vector3 originalPosition;
+    private bool moving;
+
+    private void Update()
+    {
+        if (dragging)
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var layerMask = LayerMask.GetMask("Board", "Table");
+            var offset = 0.2f;
+            if (Physics.Raycast(ray, out var hitinfo, float.MaxValue, layerMask))
+                transform.position = new Vector3(hitinfo.point.x, hitinfo.point.y + offset, hitinfo.point.z);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (!moving)
+        {
+            dragging = true;
+            originalPosition = transform.position;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (dragging)
+        {
+            dragging = false;
+            moving = true;
+            StartCoroutine(MoveToOriginalPosition());
+        }
+    }
+
+    private IEnumerator MoveToOriginalPosition()
+    {
+        var startPos = transform.position;
+        var startTime = Time.time;
+        var duration = 0.5f;
+
+        while (Time.time - startTime < duration)
+        {
+            var t = (Time.time - startTime) / duration;
+            transform.position = Vector3.Lerp(startPos, originalPosition, t);
+            yield return new WaitForEndOfFrame();
+        }
+
+        transform.position = originalPosition;
+        moving = false;
+    }
+}
