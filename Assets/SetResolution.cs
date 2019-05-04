8e8668c2149ce20b64cda7a6809fa477eaa8883f
Markus Trenkwalder <trenki2@gmx.net>
2019-05-04 17:01:02 +0200

    Add Resolution script

---
+++ Assets/SetResolution.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetResolution : MonoBehaviour
+{
+    void Update()
+    {
+        Screen.SetResolution(960, 540, false);
+    }
+}
