211fd4886ad359553c97297c27cde71bd761de42
Markus Trenkwalder <trenki2@gmx.net>
2019-05-05 10:48:15 +0200

    Added missing files

---
+++ Assets/Scripts/AIBehaviour.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class AIBehaviour : MonoBehaviour
+{
+    public int player = 2;
+
+    private enum State
+    {
+        Begin,
+        WaitForResult,
+        PlacingStone,
+        TakingStone,
+        MovingStone
+    }
+
+    private object locker = new object();
+    private State state = State.Begin;
+    private Board board;
+    private MillGame game;
+    private MillAction action;
+    private Queue<GameObject> stones = new Queue<GameObject>();
+
+    private void Start()
+    {
+        board = GameObject.Find("Board").GetComponent<Board>();
+        game = board.Game;
+
+        foreach (var obj in FindObjectsOfType<GameObject>())
+        {
+            if (obj.name.Contains("StoneBlack"))
+                stones.Enqueue(obj);
+        }
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case State.Begin:
+                if (!game.IsGameOver && game.Player == player)
+                {
+                    CalculateAction();
+                    state = State.WaitForResult;
+                }
+                break;
+
+            case State.WaitForResult:
+                if (ResultAvailable)
+                {
+                    switch (game.State)
+                    {
+                        case MillState.PlacingStones:
+                            StartCoroutine(PlaceStone());
+                            break;
+
+                        case MillState.MovingStones:
+                            StartCoroutine(MoveStone());
+                            break;
+
+                        case MillState.TakingStones:
+                            StartCoroutine(TakeStone());
+                            break;
+                    }
+                }
+                break;
+        }
+    }
+
+    private bool ResultAvailable
+    {
+        get
+        {
+            lock (locker)
+            {
+                return action != null;
+            }
+        }
+    }
+
+    private IEnumerator TakeStone()
+    {
+        state = State.TakingStone;
+
+        var stone = board.Stones[action.Pos0];
+        board.Stones[action.Pos0] = null;
+        Destroy(stone);
+        game.Execute(action);
+        Debug.Assert(game.Board[action.Pos0] == 0);
+
+        state = State.Begin;
+
+        yield return null;
+    }
+
+    private IEnumerator MoveStone()
+    {
+        state = State.MovingStone;
+
+        var stone = board.Stones[action.Pos0];
+
+        var startPos = stone.transform.position;
+        var targetPos = board.Positions[action.Pos1];
+
+        var startTime = Time.time;
+        var duration = 0.5f;
+
+        while (Time.time - startTime < duration)
+        {
+            var t = (Time.time - startTime) / duration;
+            stone.transform.position = Vector3.Lerp(startPos, targetPos, t);
+            yield return new WaitForEndOfFrame();
+        }
+
+        stone.transform.position = targetPos;
+        board.Stones[action.Pos0] = null;
+        board.Stones[action.Pos1] = stone;
+        game.Execute(action);
+        Debug.Assert(game.Board[action.Pos1] == player);
+
+        state = State.Begin;
+    }
+
+    private IEnumerator PlaceStone()
+    {
+        state = State.PlacingStone;
+
+        var stone = stones.Dequeue();
+
+        var startPos = stone.transform.position;
+        var targetPos = board.Positions[action.Pos0];
+
+        var startTime = Time.time;
+        var duration = 0.5f;
+
+        while (Time.time - startTime < duration)
+        {
+            var t = (Time.time - startTime) / duration;
+            stone.transform.position = Vector3.Lerp(startPos, targetPos, t);
+            yield return new WaitForEndOfFrame();
+        }
+
+        stone.transform.position = targetPos;
+        board.Stones[action.Pos0] = stone;
+        game.Execute(action);
+        Debug.Assert(game.Board[action.Pos0] == player);
+
+        state = State.Begin;
+    }
+
+    private void CalculateAction()
+    {
+        action = null;
+
+        Task.Run(() =>
+        {
+            var ai = new MillAI();
+            var action = ai.CalculateAction(board.Game);
+
+            lock (locker)
+            {
+                this.action = action;
+            }
+        });
+    }
+}
---
+++ Assets/Scripts/QualityAdaptation.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Automatically scales quality up or down based on the current framerate (average).
+/// </summary>
+/// 
+/// \author Kaspar Manz
+/// \date 2014-03-10
+/// \version 1.0.0
+public class QualityAdaptation : MonoBehaviour
+{
+    /// <summary>
+    /// The number of data points to calculate the average FPS over.
+    /// </summary>
+    int numberOfDataPoints;
+    /// <summary>
+    /// The current average fps.
+    /// </summary>
+    float currentAverageFps;
+    /// <summary>
+    /// The time interval in which the class checks for the framerate and adapts quality accordingly.
+    /// </summary>
+    public float TimeIntervalToAdaptQualitySettings = 10f;
+    /// <summary>
+    /// The lower FPS threshold. Decrease quality when FPS falls below this.
+    /// </summary>
+    public float LowerFPSThreshold = 25f;
+    /// <summary>
+    /// The upper FPS threshold. Increase quality when FPS is above this.
+    /// </summary>
+    public float UpperFPSThreshold = 40f;
+    /// <summary>
+    /// The stability of the current quality setting. Below 0 if changes have been
+    /// made, otherwise positive.
+    /// </summary>
+    int stability;
+    /// <summary>
+    /// Tracks whether quality was improved or worsened.
+    /// </summary>
+    bool lastMovementWasDown;
+    /// <summary>
+    /// Counter that keeps track when the script can't decide between lowering or increasing quality.
+    /// </summary>
+    int flickering;
+
+    void Start()
+    {
+        StartCoroutine(AdaptQuality());
+    }
+
+
+    void Update()
+    {
+        UpdateCumulativeAverageFPS(1 / Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Updates the cumulative average FPS.
+    /// </summary>
+    /// <param name="newFPS">New FPS.</param>
+    float UpdateCumulativeAverageFPS(float newFPS)
+    {
+        ++numberOfDataPoints;
+        currentAverageFps += (newFPS - currentAverageFps) / numberOfDataPoints;
+
+        return currentAverageFps;
+    }
+
+    /// <summary>
+    /// Sets the quality accordingly to the current thresholds.
+    /// </summary>
+    IEnumerator AdaptQuality()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(TimeIntervalToAdaptQualitySettings);
+
+            if (Debug.isDebugBuild)
+            {
+                Debug.Log("Current Average Framerate is: " + currentAverageFps);
+            }
+
+            // Decrease level if framerate too low
+            if (currentAverageFps < LowerFPSThreshold)
+            {
+                QualitySettings.DecreaseLevel();
+                --stability;
+                if (!lastMovementWasDown)
+                {
+                    ++flickering;
+                }
+                lastMovementWasDown = true;
+                if (Debug.isDebugBuild)
+                {
+                    Debug.Log("Reducing Quality Level, now " + QualitySettings.names[QualitySettings.GetQualityLevel()]);
+                }
+
+                // In case we are "flickering" (switching between two quality settings),
+                // stop it, using the lower quality level.
+                if (flickering > 1)
+                {
+                    if (Debug.isDebugBuild)
+                    {
+                        Debug.Log(string.Format(
+                          "Flickering detected, staying at {0} to stabilise.",
+                          QualitySettings.names[QualitySettings.GetQualityLevel()]));
+                    }
+                    Destroy(this);
+                }
+
+            }
+            else
+              // Increase level if framerate is too high
+              if (currentAverageFps > UpperFPSThreshold)
+            {
+                QualitySettings.IncreaseLevel();
+                --stability;
+                if (lastMovementWasDown)
+                {
+                    ++flickering;
+                }
+                lastMovementWasDown = false;
+                if (Debug.isDebugBuild)
+                {
+                    Debug.Log("Increasing Quality Level, now " + QualitySettings.names[QualitySettings.GetQualityLevel()]);
+                }
+            }
+            else
+            {
+                ++stability;
+            }
+
+            // If we had a framerate in the range between 25 and 60 frames three times
+            // in a row, we consider this pretty stable and remove this script.
+            if (stability > 3)
+            {
+                if (Debug.isDebugBuild)
+                {
+                    Debug.Log("Framerate is stable now, removing automatic adaptation.");
+                }
+                Destroy(this);
+            }
+
+            // Reset moving average
+            numberOfDataPoints = 0;
+            currentAverageFps = 0;
+        }
+    }
+}
